de3ce93e7ea54a28301703f3f5f0e667c2520d5c
LongPN <longpn.md@gmail.com>
2025-04-19 17:09:56 +0700

    update sêd

--- strapi/scripts/check-and-seed.js
+++ strapi/scripts/check-and-seed.js
@@ -1,31 +1,31 @@
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
-const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
+// const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
 
-async function checkAndSeed() {
-  try {
-    // Kiểm tra xem dữ liệu đã tồn tại hay chưa
-    const response = await fetch(`${STRAPI_URL}/api/global`);
-    if (response.ok) {
-      const data = await response.json();
-      if (data && data.data && data.data.length > 0) {
-        console.log("Data already exists. Skipping seed.");
-        return;
-      }
-    }
+// async function checkAndSeed() {
+//   try {
+//     // Kiểm tra xem dữ liệu đã tồn tại hay chưa
+//     // const response = await fetch(`${STRAPI_URL}/api/global`);
+//     // if (response.ok) {
+//     //   const data = await response.json();
+//     //   if (data && data.data && data.data.length > 0) {
+//     //     console.log("Data already exists. Skipping seed.");
+//     //     return;
+//     //   }
+//     // }
 
-    // Nếu không có dữ liệu, thực hiện seed
-    console.log("No data found. Seeding...");
-    exec("yarn strapi import -f ./data/export_20250116105447.tar.gz --force", (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error during seed: ${error.message}`);
-        return;
-      }
-      console.log(stdout);
-    });
-  } catch (error) {
-    console.error("Error checking data:", error);
-  }
-}
+//     // Nếu không có dữ liệu, thực hiện seed
+//     console.log("No data found. Seeding...");
+//     exec("yarn strapi import -f ./data/export_20250116105447.tar.gz --force", (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`Error during seed: ${error.message}`);
+//         return;
+//       }
+//       console.log(stdout);
+//     });
+//   } catch (error) {
+//     console.error("Error checking data:", error);
+//   }
+// }
 
-checkAndSeed();
+// checkAndSeed();
