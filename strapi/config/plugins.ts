8a9b0475f03a369bab8a56b5b542d9db25b0e045
LongPN <longpn.md@gmail.com>
2025-04-21 03:01:54 +0700

    move volumes to source

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+const plugins: Record<string, any> = () => ({
+  i18n: {
+    enabled: true,
+    config: {
+      defaultLocale: 'vi', // Ngôn ngữ mặc định
+      locales: ['en', 'vi', 'fr'], // Danh sách các ngôn ngữ được hỗ trợ
+    },
+  },
+});
+
+export default plugins;
